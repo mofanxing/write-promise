13045d4f3d977e8946c23315a9ea5dec282b9aa1
张荣平 <1473166181@qq.com>
2022-04-27 13:12:37 +0800

    将同步变为异步,成为微任务

--- index.js
+++ index.js
@@ -1,48 +1,67 @@
 class MyPromise {
-    static PENDING = 'pending'
-    static FULFILLED = 'fulfilled'
-    static REJECTED = 'rejected'
-    constructor(callBack) {
-        this.status = MyPromise.PENDING
-        this.value = null
+  static PENDING = "pending";
+  static FULFILLED = "fulfilled";
+  static REJECTED = "rejected";
+  constructor(callBack) {
+    this.status = MyPromise.PENDING;
+    this.value = null;
+    try {
+      callBack(this.reslove.bind(this), this.reject.bind(this));
+    } catch (error) {
+      this.reject(error);
+    }
+  }
+
+  reslove(value) {
+    if (this.status === MyPromise.PENDING) {
+      this.status = MyPromise.FULFILLED;
+      this.value = value;
+    }
+  }
+
+  reject(reason) {
+    if (this.status === MyPromise.PENDING) {
+      this.status = MyPromise.REJECTED;
+      this.value = reason;
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    if (typeof onFulfilled !== "function") {
+      onFulfilled = () => {};
+    }
+    if (typeof onRejected !== "function") {
+      onRejected = () => {};
+    }
+    if (this.status === MyPromise.FULFILLED) {
+      queueMicrotask(() => {
         try {
-            callBack(this.reslove.bind(this), this.reject.bind(this))
+          onFulfilled(this.value);
         } catch (error) {
-            this.reject(error)
+          onRejected(error);
         }
+      });
     }
 
-    reslove(value) {
-        if(this.status === MyPromise.PENDING) {
-            this.status = MyPromise.FULFILLED
-            this.value = value
+    if (this.status === MyPromise.REJECTED) {
+      queueMicrotask(() => {
+        try {
+          onRejected(this.value);
+        } catch (error) {
+          onRejected(error);
         }
-       
+      });
     }
-
-    reject(reason) {
-        if(this.status === MyPromise.PENDING) {
-            this.status = MyPromise.REJECTED
-            this.value = reason
-        }
-        
-    }
-
-    then(onFulfilled, onRejected) {
-        if(typeof onFulfilled !== 'function') {
-            onFulfilled = () => {}
-        }
-        if(typeof onRejected !== 'function') {
-            onRejected = () => {}
-        }
-        if(this.status === MyPromise.FULFILLED) {
-            onFulfilled(this.value)
-        }
-
-        if(this.status === MyPromise.REJECTED) {
-            onRejected(this.value)
-        }
-    }
-
+  }
 }
+const p = new MyPromise((reslove, reject) => {
+  reslove("解决");
+});
+p.then((res) => {
+  console.log(res);
+});
+setTimeout(() => {
+    console.log('timeout')
+})
 
+console.log("执行完成");
