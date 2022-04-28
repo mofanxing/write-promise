9fd720ad131c99e3bd491f70b2b01b824b7e6c3f
张荣平 <1473166181@qq.com>
2022-04-28 13:13:15 +0800

    pending异步任务处理

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ class MyPromise {
   constructor(execute) {
     this.status = MyPromise.PENDING;
     this.value = null;
-    this.callbacks = []
+    this.callbacks = [];
     try {
-        execute(this.reslove.bind(this), this.reject.bind(this));
+      execute(this.reslove.bind(this), this.reject.bind(this));
     } catch (error) {
       this.reject(error);
     }
@@ -17,9 +17,11 @@ class MyPromise {
     if (this.status === MyPromise.PENDING) {
       this.status = MyPromise.FULFILLED;
       this.value = value;
-      this.callbacks.forEach(callback => {
-        callback.onFulfilled(value)
-      })
+      queueMicrotask(() => {
+        this.callbacks.forEach((callback) => {
+          callback.onFulfilled(value);
+        });
+      });
     }
   }
 
@@ -27,9 +29,11 @@ class MyPromise {
     if (this.status === MyPromise.PENDING) {
       this.status = MyPromise.REJECTED;
       this.value = reason;
-      this.callbacks.forEach(callback => {
-          callback.onRejected(reason)
-      })
+      queueMicrotask(() => {
+        this.callbacks.forEach((callback) => {
+          callback.onRejected(reason);
+        });
+      });
     }
   }
 
@@ -41,30 +45,29 @@ class MyPromise {
       onRejected = () => {};
     }
 
-    if(this.status === MyPromise.PENDING) {
-        this.callbacks.push({
-            onFulfilled: value => {
-                try {
-                    onFulfilled(value)
-                } catch (error) {
-                    onRejected(error)
-                }
-            },
-            onRejected: value => {
-                try {
-                    onRejected(value)
-                } catch (error) {
-                    onRejected(error)
-                }
-            }
-        })
+    if (this.status === MyPromise.PENDING) {
+      this.callbacks.push({
+        onFulfilled: (value) => {
+          try {
+            onFulfilled(value);
+          } catch (error) {
+            onRejected(error);
+          }
+        },
+        onRejected: (value) => {
+          try {
+            onRejected(value);
+          } catch (error) {
+            onRejected(error);
+          }
+        },
+      });
     }
 
     if (this.status === MyPromise.FULFILLED) {
-       
       queueMicrotask(() => {
         try {
-            onFulfilled(this.value);
+          onFulfilled(this.value);
         } catch (error) {
           onRejected(error);
         }
@@ -84,16 +87,21 @@ class MyPromise {
 }
 const p = new MyPromise((reslove, reject) => {
   setTimeout(() => {
-    reject('拒绝')
-  },1000)
+    reject("拒绝");
+    console.log('sss')
+  }, 1000);
 });
-p.then((res) => {
-  console.log(res);
-}, rej => {
-    console.log(rej)
-});
+p.then(
+  (res) => {
+    console.log(res);
+  },
+  (rej) => {
+    console.log(rej);
+  }
+);
+
 setTimeout(() => {
-    console.log('timeout')
-})
+  console.log("timeout");
+});
 
 console.log("执行完成");
